ac9b4bd5f1789fef86c54fd00ae905b154a4eb88
Natnael Eshetu <natieshzed@gmail.com>
2026-04-07 16:55:11 +0300

    move defaultPricingPlans to src/data

--- backend/db/index.ts
+++ backend/db/index.ts
@@ -3,11 +3,10 @@ import { drizzle as drizzlePG } from "drizzle-orm/node-postgres";
 import { Pool } from "pg";
 import { drizzle as drizzleSQLite } from "drizzle-orm/bun-sqlite";
 import { Database } from "bun:sqlite";
-import UserService from "~/services/user.service";
 import pgSchema from "./schema/pg";
 import sqliteSchema from "./schema/sqlite";
 import PaymentService from "~/services/payment.service";
-import { PricingPlanInit } from "~/base";
+import defaultPricingPlans from "@/data/default-pricing-plans";
 
 const pool = new Pool({
   connectionString: dbConfig.pgDatabaseURL,
@@ -78,75 +77,3 @@ export const initDb = async () => {
     }
   }
 };
-
-const defaultPricingPlans: PricingPlanInit[] = [
-  {
-    id: "free",
-    name: "Free",
-    price: 0,
-    maxServices: 1,
-    maxEmployees: 10,
-    monthlyDiscount: 0,
-    annualDiscount: 0,
-    features: [
-      "1 service included",
-      "Up to 10 employees",
-      "Attachments",
-      "Basic analytics",
-    ],
-    popular: false,
-  },
-  {
-    id: "small",
-    name: "Small Business",
-    price: 1000,
-    monthlyDiscount: 0.999,
-    annualDiscount: 0.83325,
-    maxServices: 3,
-    maxEmployees: 30,
-    features: [
-      "3 services included",
-      "Up to 30 employees",
-      "Attachments",
-      "Working hours phone support",
-      "Advanced analytics",
-    ],
-    popular: true,
-  },
-  {
-    id: "medium",
-    name: "Medium Business",
-    price: 3000,
-    monthlyDiscount: 0.9998,
-    annualDiscount: 0.8333,
-    maxServices: 20,
-    maxEmployees: 200,
-    features: [
-      "20 services included",
-      "Up to 200 employees",
-      "Attachments",
-      "Working hours phone support",
-      "Advanced analytics",
-      "Payment Integration",
-    ],
-    popular: false,
-  },
-  {
-    id: "large",
-    name: "Large Business",
-    price: 10000,
-    monthlyDiscount: 0.9999,
-    annualDiscount: 0.833327,
-    maxServices: 100,
-    maxEmployees: 1000,
-    features: [
-      "100 services included",
-      "Up to 1000 employees",
-      "Attachments",
-      "Working hours phone support",
-      "Advanced analytics",
-      "Payment Integration",
-    ],
-    popular: false,
-  },
-];
