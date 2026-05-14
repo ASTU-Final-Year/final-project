a172bc94ad101dd0ab6239116d9345937fc1a540
Sisay Derese <deresesisay748@gmail.com>
2026-05-14 06:09:38 -0700

    client profile

--- src/components/app-sidebar-client.jsx
+++ src/components/app-sidebar-client.jsx
@@ -14,6 +14,7 @@ import {
   UserPlus,
   Plus,
   Activity,
+  User,
 } from "lucide-react";
 
 import {
@@ -54,7 +55,12 @@ const navData = [
   //   title: "Analytics",
   //   url: "/dashboard/employee/analytics",
   //   icon: PieChart,
-  // },
+  // },},
+  {
+    title: "Profile",
+    url: "/dashboard/client/profile",
+    icon: User,
+  },
 ];
 
 export function AppSidebarClient() {
